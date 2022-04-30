4647e86e8ff7535f70ba8074d62568e8c747ab3a
jose-Menard <3021892970101@ingenieria.usac.edu.gt>
2022-04-29 22:53:13 -0600

    examen final terminado

--- src/examenfinal/ExamenFinal.java
+++ src/examenfinal/ExamenFinal.java
@@ -1,7 +1,6 @@
-
 package examenfinal;
 import java.util.Scanner;
-import java.util.Random;
+
 /**
  *
  * @author Jose Pablo
@@ -28,22 +27,20 @@ public class ExamenFinal {
             System.out.println("4. Salir");
 
 
-
-            System.out.println("Introduzca una opción");
+            System.out.println();
+            System.out.println("-----Introduzca una opción------");
             opcion = sn.nextInt();
 
             switch(opcion) {
                 case 1:
                     Scanner entrada = new Scanner (System.in);
                                       
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
+                    System.out.println();
+ 
            
                     
-                    System.out.println("Problema 1");
-                    
+                    System.out.println("---Problema 1---");
+                    System.out.println();
                     System.out.println("favor ingresar un número");
                     a = entrada.nextInt();
                     
@@ -56,23 +53,19 @@ public class ExamenFinal {
                     System.out.println("el número mayor es: " +b);
                     }
                     
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
-           
+                    System.out.println();
+
                             break;
 
 
                 case 2:
                                    
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
+                    System.out.println();
+
            
                     
-                    System.out.println("Problema 2");
+                    System.out.println("---Problema 2---");
+                    System.out.println();
                     int filas;
                     Scanner abajo=new Scanner (System.in);
                     System.out.println("Favor ingresar la cantidad de asteríscos inferiores (IMPAR)");
@@ -94,32 +87,67 @@ public class ExamenFinal {
                         System.out.println();
                     }        
                     }
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
+                    System.out.println();
+
 
 
                     break;
                     
                     case 3:
                         
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
+                    System.out.println();
+ 
                
-                    System.out.println("Problema 3");
+                    System.out.println("---Problema 3---");
+                    System.out.println();
+                     int vacas[]=new int[8];
+                     int vacas2[]=new int[8];
+                     int peso[]=new int[8];
+                     int leche[]=new int[8];
+                     int lecheTotal;
+                     int vacasBuenas[]=new int[8];
+                     
+                     
+                     leche[0]=30;
+                     leche[1]=34;
+                     leche[2]=28;
+                     leche[3]=45;
+                     leche[4]=31;
+                     leche[5]=50;
+                     leche[6]=29;
+                     leche[7]=1;
+                     
+                       for (int r=0; r<vacas.length; r++){
+                        vacas[r]=leche[r]; 
+                        }
+                       
+                     peso[0]=223;
+                     peso[1]=243;
+                     peso[2]=100;
+                     peso[3]=200;
+                     peso[4]=200;
+                     peso[5]=155;
+                     peso[6]=300;
+                     peso[7]=150;
+                     
+                      for (int r=0; r<vacas2.length; r++){
+                        vacas2[r]=peso[r]; 
+                        }
+                      
+                      for (int r=0; r<vacas2.length; r++){
+                          if(vacas[r]>28 && vacas2[r]>=200){
+                              vacasBuenas[r]=vacas[r];
+                      }    
+                        }
+                      
+                      lecheTotal=vacasBuenas[0]+vacasBuenas[1]+vacasBuenas[2]+vacasBuenas[3]+vacasBuenas[4]+vacasBuenas[5];
+                      
+                      System.out.println("Las Vaquitas menos pesadas producirán: " +lecheTotal+" litros de leche");
 
-
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
-                    System.out.println("");
-
-
+                        System.out.println();
+                        System.out.println("Una disculpa, esto es lo que logré hacer con el tiempo que tenía :(");
+                        System.out.println();
                     break;
-
                 case 4:
                     salir=true;
                     break;
