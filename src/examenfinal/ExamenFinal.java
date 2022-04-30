9cbdac4b878fa7ca31af1df03fed721badff4c7b
jose-Menard <3021892970101@ingenieria.usac.edu.gt>
2022-04-29 21:13:53 -0600

    menú y ejercicio 1 completado

--- src/examenfinal/ExamenFinal.java
+++ src/examenfinal/ExamenFinal.java
@@ -1,9 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package examenfinal;
 
+package examenfinal;
+import java.util.Scanner;
+import java.util.Random;
 /**
  *
  * @author Jose Pablo
@@ -15,6 +13,107 @@ public class ExamenFinal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
-}
+
+        Scanner sn = new Scanner(System.in);
+        boolean salir = false;
+        int opcion;
+        
+        int a;
+        int b;
+        while(!salir){
+
+            System.out.println("1. Problema 1");
+            System.out.println("2. Problema 2");
+            System.out.println("3. Problema 3");
+            System.out.println("4. Salir");
+
+
+
+            System.out.println("Introduzca una opción");
+            opcion = sn.nextInt();
+
+            switch(opcion) {
+                case 1:
+                    Scanner entrada = new Scanner (System.in);
+                                      
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+           
+                    
+                    System.out.println("Problema 1");
+                    
+                    System.out.println("favor ingresar un número");
+                    a = entrada.nextInt();
+                    
+                    System.out.println("favor ingresar otro número");
+                    b = entrada.nextInt();
+                    
+                    if(a>b){
+                    System.out.println("el número mayor es: " +a);
+                    }else{
+                    System.out.println("el número mayor es: " +b);
+                    }
+                    
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+           
+                            break;
+
+
+                case 2:
+                                   
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+           
+                    
+                    System.out.println("Problema 2");
+
+
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+
+
+                    break;
+                    
+                    case 3:
+                        
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+               
+                    System.out.println("Problema 3");
+
+
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+                    System.out.println("");
+
+
+                    break;
+
+                case 4:
+                    salir=true;
+                    break;
+                default:
+                    System.out.println("Las opciones son entre 1 y 4");
+
+           System.out.println("");
+           System.out.println("");
+           System.out.println("");
+           System.out.println("");
+           System.out.println("");
+            }
+            }
+            }
+        }
+
